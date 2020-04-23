7eac0daa901419c24d77aeef8a0c959c98604bed
Alexandre Pellegrino <alesirius@gmail.com>
2020-04-22 23:17:59 -0300

    first commits

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { useRouteMatch } from 'react-router-dom';
+
+const Repository: React.FC = () => (
+  <>
+    <h1>Repository</h1>
+  </>
+);
+
+export default Repository;
